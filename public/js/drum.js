f530f6738e0050859e102f5422773140fb9741e8
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-01-18 15:10:54 -0500

    connected audio to keylistener, and set sounds to be played repeatedly over eachoter

--- public/js/drum.js
+++ public/js/drum.js
@@ -1,3 +1,8 @@
 window.addEventListener('keydown', function (e) {
-    console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stop the function from running all together
+
+    audio.currentTime = 0; // rewind to start of sample
+    audio.play();
+    console.log(audio);
 });
